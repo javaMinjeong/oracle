013536f28068ab60a0e91e67d4e8f56ca548ef01
javaMinjeong <mmjjkk01234@naver.com>
2023-08-29 17:42:26 +0900

    문제 풀이

--- 문제03.sql
+++ 문제03.sql
@@ -8,14 +8,29 @@
 
 -- 1. tblCountry. 아시아(AS)와 유럽(EU)에 속한 나라의 개수?? -> 7개
         
-
+	SELECT
+		count(*)
+	FROM tblcountry
+		WHERE continent = 'AS' AND continent = 'EU';
+	
 -- 2. 인구수가 7000 ~ 20000 사이인 나라의 개수?? -> 2개
     
+	SELECT 
+		count(CASE
+			WHERE population = 7000 BETWEEN population = 20000
+		END) 
+	FROM tblcountry;
+	
 
 
 -- 3. hr.employees. job_id > 'IT_PROG' 중에서 급여가 5000불이 넘는 직원이 몇명? -> 2명
-    
-
+    SELECT
+	  job_id,
+	  CASE
+	  	WHERE job_id = 'IT_PROG' THEN 1 
+	  END
+    FROM employees;
+--		WHERE salary > 5000;
 
 -- 4. tblInsa. tel. 010을 안쓰는 사람은 몇명?(연락처가 없는 사람은 제외) -> 42명
     
@@ -23,11 +38,26 @@
     
 
 -- 5. city. 서울, 경기, 인천 -> 그 외의 지역 인원수? -> 18명
-
+	SELECT
+		city,	
+		CASE
+			WHERE city = '서울' THEN 1
+		END
+	FROM employees;
     
 
 -- 6. 여름태생(7~9월) + 여자 직원 총 몇명? -> 7명
 
 
 -- 7. 개발부 + 직위별 인원수? -> 부장 ?명, 과장 ?명, 대리 ?명, 사원 ?명
-
+	SELECT
+	count(*) 
+	count(
+		CASE
+			
+			
+		END
+		
+	)
+	FROM tblinsa
+		WHERE buseo = '개발부';
--- 문제04.sql
+++ 문제04.sql
@@ -7,39 +7,80 @@
 --sum()
 --1. 유럽과 아프리카에 속한 나라의 인구 수 합? tblCountry > 14,198
 	
-	
+	SELECT 
+		sum(population)
+	FROM tblcountry
+		WHERE continent IN ('EU','AF');
 
 --2. 매니저(108)이 관리하고 있는 직원들의 급여 총합? hr.employees > 39,600
 
+SELECT
+	sum(salary)
+FROM employees
+	WHERE manager_id = '108' ;
 
 --3. 직업(ST_CLERK, SH_CLERK)을 가지는 직원들의 급여 합? hr.employees > 120,000
 
+SELECT 
+	sum(salary)
+FROM employees
+	WHERE job_id IN ('ST_CLERK','SH_CLERK'); 
 
 --4. 서울에 있는 직원들의 급여 합(급여 + 수당)? tblInsa > 33,812,400
 
+SELECT
+	sum(basicpay + sudang)
+FROM tblinsa
+	WHERE city = '서울';
 
 --5. 장급(부장+과장)들의 급여 합? tblInsa > 36,289,000
 
+SELECT
+	sum(basicpay)
+FROM tblinsa
+ 	WHERE jikwi IN ('부장','과장');
 
 
 --avg()
 --1. 아시아에 속한 국가의 평균 인구수? tblCountry > 39,165
 	
+ SELECT 
+ 	avg(population)
+ FROM tblcountry
+ 	WHERE continent = 'AS';
 
---2. 이름(first_name)에 'AN'이 포함된 직원들의 평균 급여?(대소문자 구분없이) hr.employees > 6,270.4
+--**2. 이름(first_name)에 'AN'이 포함된 직원들의 평균 급여?(대소문자 구분없이) hr.employees > 6,270.4
 
+ SELECT 
+-- 	first_name,
+ 	avg(salary)
+ FROM employees
+ 	WHERE lower(first_name) = '%an%';
+ 
+ 
 
 --3. 장급(부장+과장)의 평균 급여? tblInsa > 2,419,266.66
 	SELECT
-		
+		trunc(avg(basicpay),2)	
 	FROM tblinsa
-	
+		WHERE jikwi IN ('부장','과장');
 
 --4. 사원급(대리+사원)의 평균 급여? tblInsa > 1,268,946.66
 
+	SELECT
+		trunc(avg(basicpay), 2)
+	FROM tblinsa
+		WHERE jikwi IN	('대리','사원');
+	
 
---5. 장급(부장,과장)의 평균 급여와 사원급(대리,사원)의 평균 급여의 차액? tblInsa > 1,150,320
-
+**--5. 장급(부장,과장)의 평균 급여와 사원급(대리,사원)의 평균 급여의 차액? tblInsa > 1,150,320
+--case end...?
+	SELECT
+		avg(basicpay)
+	FROM tblinsa
+		WHERE 	
+		
+		
 
 --max(),min()
 --1. 면적이 가장 넓은 나라의 면적은? tblCountry > 959
